9937f6c4fabde94159b79af7b85a1525a893c419
aadarsh <adarshgautam23@gmail.com>
2022-08-22 20:13:21 +0545

    NewReport

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,23 +1,49 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Label, ResponsiveContainer,CartesianGrid,Tooltip } from 'recharts';
 import Title from './Title';
+import { Column } from '@ant-design/plots';
+import { ResetTvRounded } from '@mui/icons-material';
+import ColumnChart from '@ant-design/plots/es/components/column';
+import ReactDOM from 'react-dom';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
+function createData(type, Followers) {
+  return { type, Followers };
 }
 
 const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+  {
+    type: 'Mahabir',
+    Followers: 50,
+  },
+  {
+    type: 'Kantipur',
+    Followers: 52,
+  },
+  {
+    type: 'Balen',
+    Followers: 61,
+  },
+  {
+    type: 'Baburam',
+    Followers: 145,
+  },
+  {
+    type: 'Gagan',
+    Followers: 73,
+  },
+  {
+    type: 'Amrit',
+    Followers: 38,
+  },
+  {
+    type: 'Setopati',
+    Followers: 49,
+  },
+  {
+    type: 'RONB',
+    Followers: 85,
+  },
 ];
 
 export default function Chart() {
@@ -27,25 +53,34 @@ export default function Chart() {
     <React.Fragment>
       <Title>Today</Title>
       <ResponsiveContainer>
-        <LineChart
+        <BarChart
+        
           data={data}
           margin={{
             top: 16,
             right: 16,
             bottom: 0,
-            left: 24,
-          }}
+            left: 16,  
+            
+
+          }} 
         >
           <XAxis
-            dataKey="time"
+            dataKey="type"
             stroke={theme.palette.text.secondary}
             style={theme.typography.body2}
+            Strock="#8884d8" 
           />
+
           <YAxis
+          dataKey="Followers"
             stroke={theme.palette.text.secondary}
             style={theme.typography.body2}
+            
           >
-            <Label
+           
+      
+            <Label 
               angle={270}
               position="left"
               style={{
@@ -53,20 +88,27 @@ export default function Chart() {
                 fill: theme.palette.text.primary,
                 ...theme.typography.body1,
               }}
+              
             >
-              Sales ($)
+              Twitter
             </Label>
           </YAxis>
-          <Line
-            isAnimationActive={false}
-            type="monotone"
-            dataKey="amount"
+          <Tooltip />
+           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <Bar
+         
+            dataKey="Followers"
             stroke={theme.palette.primary.main}
-            dot={false}
+            fill="#8884d8"  
+            barSize={40}
           />
-        </LineChart>
+       
+       return <Column {...useTheme} />;
+        </BarChart>
+       
       </ResponsiveContainer>
     </React.Fragment>
   );
 }
+
 
