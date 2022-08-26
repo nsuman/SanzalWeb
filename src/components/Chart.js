7cdbec9283d100709e2fd21f07e54407abca4431
nsuman <nsuman@mit.edu>
2022-08-25 22:21:30 -0400

    ant design chart changes

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,114 +1,53 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { BarChart, Bar, XAxis, YAxis, Label, ResponsiveContainer,CartesianGrid,Tooltip } from 'recharts';
-import Title from './Title';
 import { Column } from '@ant-design/plots';
-import { ResetTvRounded } from '@mui/icons-material';
-import ColumnChart from '@ant-design/plots/es/components/column';
-import ReactDOM from 'react-dom';
-
-function createData(type, Followers) {
-  return { type, Followers };
-}
 
 const data = [
   {
-    type: 'Mahabir',
-    Followers: 50,
+    name: 'Mahabir',
+    followers: 50,
   },
   {
-    type: 'Kantipur',
-    Followers: 52,
+    name: 'Kantipur',
+    followers: 52,
   },
   {
-    type: 'Balen',
-    Followers: 61,
+    name: 'Balen',
+    followers: 61,
   },
   {
-    type: 'Baburam',
-    Followers: 145,
+    name: 'Baburam',
+    followers: 145,
   },
   {
-    type: 'Gagan',
-    Followers: 73,
+    name: 'Gagan',
+    followers: 73,
   },
   {
-    type: 'Amrit',
-    Followers: 38,
+    name: 'Amrit',
+    followers: 38,
   },
   {
-    type: 'Setopati',
-    Followers: 49,
+    name: 'Setopati',
+    followers: 49,
   },
   {
-    type: 'RONB',
-    Followers: 85,
+    name: 'RONB',
+    followers: 85,
   },
 ];
 
-export default function Chart() {
-  const theme = useTheme();
-
-  return (
-    <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <BarChart
-        
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 16,  
-            
-
-          }} 
-        >
-          <XAxis
-            dataKey="type"
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
-            Strock="#8884d8" 
-          />
-
-          <YAxis
-          dataKey="Followers"
-            stroke={theme.palette.text.secondary}
-            style={theme.typography.body2}
-            
-          >
-           
-      
-            <Label 
-              angle={270}
-              position="left"
-              style={{
-                textAnchor: 'middle',
-                fill: theme.palette.text.primary,
-                ...theme.typography.body1,
-              }}
-              
-            >
-              Twitter
-            </Label>
-          </YAxis>
-          <Tooltip />
-           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <Bar
-         
-            dataKey="Followers"
-            stroke={theme.palette.primary.main}
-            fill="#8884d8"  
-            barSize={40}
-          />
-       
-       return <Column {...useTheme} />;
-        </BarChart>
-       
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
-}
-
+export default function Chart()  {
+  const config = {
+    data,
+    xField: 'name',
+    yField: 'followers',
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+  };
+  return <Column {...config} />;
+};
 
