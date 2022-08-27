58f47a7eb031f79f43900e009193a196b642af26
nsuman <nsuman@mit.edu>
2022-08-27 10:26:19 -0400

    bar graph added

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,53 +1,67 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Column } from '@ant-design/plots';
+import Paper from '@mui/material/Paper';
+export default function Chart() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    asyncFetch();
+  }, []);
 
-const data = [
-  {
-    name: 'Mahabir',
-    followers: 50,
-  },
-  {
-    name: 'Kantipur',
-    followers: 52,
-  },
-  {
-    name: 'Balen',
-    followers: 61,
-  },
-  {
-    name: 'Baburam',
-    followers: 145,
-  },
-  {
-    name: 'Gagan',
-    followers: 73,
-  },
-  {
-    name: 'Amrit',
-    followers: 38,
-  },
-  {
-    name: 'Setopati',
-    followers: 49,
-  },
-  {
-    name: 'RONB',
-    followers: 85,
-  },
-];
-
-export default function Chart()  {
-  const config = {
-    data,
-    xField: 'name',
-    yField: 'followers',
-    xAxis: {
-      label: {
-        autoHide: true,
-        autoRotate: false,
-      },
-    },
+  const asyncFetch = () => {
+    fetch('https://raw.githubusercontent.com/nsuman/SanzalWeb/main/src/user_data.json')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => {
+        console.log('fetch data failed', error);
+      });
   };
-  return <Column {...config} />;
+
+  const getConfig = (data, xField, yField) => {
+    return {
+      data: data.sort((a, b) => b[yField] - a[yField]).slice(0, 30),
+      xField: xField,
+      yField: yField,
+      label: {
+        content: null
+      },
+      xAxis: {
+        label: {
+          autoHide: false,
+          autoRotate: true,
+        },
+        title: {
+          text: `${xField}`
+        }
+      },
+      yAxis: {
+        title: {
+          text: `account with most  ${yField}`
+        }
+      }
+    };
+  }
+
+  return <div>
+    <Paper
+      sx={{
+        p: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'auto',
+        marginBottom: '5px'
+      }}
+    >    <Column {...getConfig(data, 'username', 'followers_count')} /></Paper>
+
+    <Paper
+      sx={{
+        p: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'auto',
+      }}
+    >    <Column {...getConfig(data, 'username', 'following_count')} /></Paper>
+  </div>
+
 };
 
