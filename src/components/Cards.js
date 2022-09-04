3b74eabf4842903ed6f9bcd61bccf4442e216df4
nsuman <nsuman@mit.edu>
2022-09-03 23:55:39 -0400

    demo commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,44 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-export default function MediaCard({img, username, numOfFollowers, index}) {
+
+export default function MediaCard({ img, username, paramCount, name, index }) {
   return (
-    <Card sx={{ maxWidth: 340 }}>
-      <CardMedia component="img" height="190" image={img} alt="green iguana" />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {`${index}. ${numOfFollowers} followers`}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          @{username.toString()}
-        </Typography>
+    <Card sx={{ height: 130 }}>
+      <CardContent sx={{ display: 'flex' }}>
+        <div style={{ maxWidth: '33%', marginRight: '10px' }}>
+          <Typography variant="h4" color="text.secondary">
+            {index.toString()}
+          </Typography>
+        </div>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', justifyContent:"space-between" }}>
+            <div>
+            <Typography variant="h5">
+              {`${paramCount}`}
+            </Typography>
+            </div>
+          <div>
+          <Typography variant="body2" color="text.secondary">
+              {name.toString()}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              @{username.toString()}
+            </Typography>
+            </div>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'column', float: 'right' }}>
+          <CardMedia component="img" image={img} sx={{ maxWidth: 75, maxHeight: 75, borderRadius: 2 }} />
+          </div>
+        </Grid>
       </CardContent>
     </Card>
   );
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,79 +1,76 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Card from '../components/Cards';
+import MediaCard from '../components/Cards';
 import Grid from '@mui/material/Grid';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Button from '@mui/material/Button';
+import ControlledOpenSelect from '../components/Select';
 
 export default function Users() {
-    const [high, setHigh] = React.useState(true); 
-    const [param, setParam] = React.useState('followers');
+  const [isDecending, setDecending] = React.useState('descending');
+  const [param, setParam] = React.useState('followers');
 
-    const rerender = React.useRef(0)
-    rerender.current += 1
-    console.log(high, param);
-    const data = React.useRef([])
+  const rerender = React.useRef(0)
+  rerender.current += 1
+  const data = React.useRef([])
 
-    if (high) {
-        if (param === 'tweet') {
-            data.current = data.current.sort((a,b) => b.tweet_count - a.tweet_count)
-        } else if (param === 'followers') {
-            data.current = data.current.sort((a,b) => b.followers_count - a.followers_count)
-        } else {
-            data.current = data.current.sort((a,b) => b.following_count - a.following_count)
-        }
+  if (isDecending === 'descending') {
+    if (param === 'tweet') {
+      data.current = data.current.sort((a, b) => b.tweet_count - a.tweet_count)
+    } else if (param === 'followers') {
+      data.current = data.current.sort((a, b) => b.followers_count - a.followers_count)
     } else {
-        if (param === 'tweet') {
-            data.current = data.current.sort((a,b) => a.tweet_count - b.tweet_count)
-        } else if (param === 'followers') {
-            data.current =data.current.sort((a,b) => a.followers_count - b.followers_count)
-        } else {
-            data.current =data.current.sort((a,b) => a.following_count - b.following_count)
-        }
+      data.current = data.current.sort((a, b) => b.following_count - a.following_count)
     }
-    React.useEffect(() => {
-      fetch('https://raw.githubusercontent.com/nsuman/SanzalWeb/demog/src/user_data.json')
+  } else {
+    if (param === 'tweet') {
+      data.current = data.current.sort((a, b) => a.tweet_count - b.tweet_count)
+    } else if (param === 'followers') {
+      data.current = data.current.sort((a, b) => a.followers_count - b.followers_count)
+    } else {
+      data.current = data.current.sort((a, b) => a.following_count - b.following_count)
+    }
+  }
+
+  React.useEffect(() => {
+    fetch('https://raw.githubusercontent.com/nsuman/SanzalWeb/demog/src/user_data.json')
       .then((response) => response.json())
       .then((json) => {
-        data.current = json.sort((a,b) => b.followers_count - a.followers_count)})
+        data.current = json.sort((a, b) => b.followers_count - a.followers_count)
+      })
       .catch((error) => {
         console.log('fetch data failed', error);
       });
-    }, []);
+  }, []);
 
-    const cards = data.current.map((element, i) => {
-        return <Card paramCount={element[`${param}_count`]} param={param} img={element.profile_image_url.replace('_normal', '') } index={i} username={element.username}/>
-    })
+  const cards = data.current.map((element, i) => {
+    return <MediaCard name={element.name} paramCount={element[`${param}_count`]} param={param} img={element.profile_image_url.replace('_normal', '_bigger')} index={i + 1} username={element.username} />
+  })
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
- <ButtonGroup
-    disableElevation
-    variant="contained"
-    aria-label="Disabled elevation buttons"
-  >
-      <Button  onClick={() => setHigh(false)}>Show Low</Button>
-    <Button onClick={() => setHigh(true)}>Show High</Button>
-  
-  </ButtonGroup>
+      <div style={{display: 'flex'}}>
+      <ControlledOpenSelect 
+        id="sortorder"
+        selectedValue={isDecending} 
+        setSelectedValue={setDecending} 
+        menuItems={[{ id: 'ascending', label: 'Low To High'}, {id: 'descending', label: 'High to Low'}]}
+        label="Order"
+      />
 
-<div style={{width: '10px'}}></div>
-  <ButtonGroup
-    disableElevation
-    variant="contained"
-    aria-label="Disabled elevation buttons"
-  >
-    <Button onClick={() => setParam('followers')}>Followers Count</Button>
-    <Button  onClick={() => setParam('following')}>Following Count</Button>
-    <Button  onClick={() => setParam('tweet')}>Tweet Count</Button>
-  </ButtonGroup>
+      <ControlledOpenSelect 
+        id="sortby"
+        selectedValue={param} 
+        setSelectedValue={setParam} 
+        menuItems={[{id: 'followers', label: 'followers'} , { id: 'following', label: 'following'}, {id: 'tweet', label: 'tweet'}]}
+        label="By"
+      />
+      </div>
 
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-  {cards.map((element, index) => (
-    <Grid item xs={2} sm={4} md={4} key={index}>
-      {element}
-    </Grid>
-  ))}
-</Grid>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {cards.map((element, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            {element}
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 };
